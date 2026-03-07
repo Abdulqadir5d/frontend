cc397fe9406d4aa00d92d3a1ca9e0695b1bace1a
[ABDUL QADIR] <159106895+Abdulqadir5d@users.noreply.github.com>
2026-03-07 07:14:02 +0500

    View

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -70,7 +70,7 @@ export default function AnalyticsPage() {
             <AnalyticsStatCard title="Total Patients" value={adminData.totalPatients} trend="+12%" icon="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
             <AnalyticsStatCard title="Staff Count" value={adminData.totalDoctors} icon="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
             <AnalyticsStatCard title="Monthly Appts" value={adminData.appointmentsThisMonth} trend="+5%" icon="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-            <AnalyticsStatCard title="Total Revenue" value={`₹${(adminData.revenueThisMonth || 0).toLocaleString()}`} icon="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2" highlight />
+            <AnalyticsStatCard title="Total Revenue" value={`₹${(adminData.revenueThisMonth || 0).toLocaleString()}`} icon="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2" />
           </>
         )}
         {isDoctor && doctorData && (
